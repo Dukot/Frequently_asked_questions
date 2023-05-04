0a2251560ed243dd28d4b03bb310419c93f19854
Bernardo Grabowski Costa <96502871+Dukot@users.noreply.github.com>
2023-05-03 21:18:39 -0300

    Finish

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+// 1- Pegar os elementos
 const accordionItems = document.querySelectorAll('.accordion-item-header');
 
+// 2- Adicionar um evento de click onde o mesmo possa abrir efechar a resposta.
 accordionItems.forEach(item => {
   item.addEventListener('click', () => {
     const currentItem = item.parentNode;
